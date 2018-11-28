f29a4aa69d8a91d9e7a1d384500b16016881f772
Trevor Neidlinger <trevorneidlinger@Trevors-MacBook-Pro.local>
2018-11-28 15:35:45 -0500

    add food when one is taken away

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
 
+//if client requests nothing by default it goes to index.html
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/client/index.html');
 });
 
+//client can only access stuff in client folder
 app.use('/client', express.static(__dirname + '/client'));
 
 serv.listen(2000);
+console.log("localhost:2000")
