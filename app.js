93fcccf8eb5ce22538b3e8910f18178fd31a9e90
Trevor Neidlinger <trevorneidlinger@Trevors-MacBook-Pro.local>
2018-11-28 16:16:18 -0500

    changed stuff for server

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var serv = require('http').Server(app);
+/*var serv = require('http').Server(app);
 
 //if client requests nothing by default it goes to index.html
 app.get('/', function(req, res){
@@ -11,4 +11,30 @@ app.get('/', function(req, res){
 app.use('/client', express.static(__dirname + '/client'));
 
 serv.listen(2000);
-console.log("localhost:2000")
+console.log("localhost:2000")*/
+
+var server = app.listen(process.env.PORT || 2000, listen);
+
+function listen() {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("Example app listening at http://" + host + ":" + port);
+}
+
+app.use(express.static('client'));
+
+var io = require('socket.io')(server);
+
+// Register a callback function to run when we have an individual connection
+// This is run for each individual user that connects
+io.sockets.on('connection',
+  // We are given a websocket object in our function
+  function(socket) {
+
+    console.log("Connected: " + socket.id);
+
+    socket.on('disconnect', function() {
+      console.log("Client has disconnected");
+    });
+  }
+);
