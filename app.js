d0e851c0b9576ae9abc1f6e88962c4e380cdb1db
Trevor Neidlinger <trevorneidlinger@Trevors-MacBook-Pro.local>
2018-11-28 16:19:06 -0500

    deleting old code on server

--- app.js
+++ app.js
@@ -1,18 +1,7 @@
 var express = require('express');
 var app = express();
-/*var serv = require('http').Server(app);
 
-//if client requests nothing by default it goes to index.html
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/client/index.html');
-});
-
-//client can only access stuff in client folder
-app.use('/client', express.static(__dirname + '/client'));
-
-serv.listen(2000);
-console.log("localhost:2000")*/
-
+//process.env.PORT used with deploying on heroku
 var server = app.listen(process.env.PORT || 2000, listen);
 
 function listen() {
