4bb5d9ffdabbb9fb0f65a4b6a4f138f0e832a2a4
Trevor Neidlinger <trevorneidlinger@Trevors-MacBook-Pro.local>
2018-12-02 23:07:21 -0500

    multiple clients can now connect to the server and see one another

--- client/circle.js
+++ client/circle.js
@@ -1,9 +1,9 @@
 //class for any circle on screen (Player Circles and food)
 class Circle {
-    constructor(x, y, r) {
+    constructor(x, y, r, health) {
         this.pos = createVector(x, y);
         this.r = r;
-        this.health = 100;
+        this.health = health;
     }
     
     update() {
@@ -41,10 +41,10 @@ class Circle {
         this.r = sqrt(newArea / PI);*/
   
         //updates objects health if food is eaten
-        if(food.r == 15){
+        if(food.r == 16){
           this.health = this.health + 15;
         } else {
-          this.health = this.health + 5;
+          this.health = this.health - 5;
         }
         return true;
       }
@@ -52,22 +52,23 @@ class Circle {
     }
 
     constrain(){
-      circle.pos.x = constrain(circle.pos.x, -width*4, width*4);
-      circle.pos.y = constrain(circle.pos.y, -height*4, height*4);
+      circle.pos.x = constrain(circle.pos.x, -2000, 2000);
+      circle.pos.y = constrain(circle.pos.y, -2000, 2000);
     }
   
     showCircle() {
-        fill(200,123,33);
-        ellipse(this.pos.x, this.pos.y, this.r * 2, this.r * 2);
+      fill(200,123,33);
+      ellipse(this.pos.x, this.pos.y, this.r * 2, this.r * 2);
     }
     showFood(){
       fill(255);
-      ellipse(this.pos.x, this.pos.y, this.r * 2, this.r * 2);
+      rect(this.pos.x, this.pos.y, this.r * 2, this.r * 2);
     }
   
     showHealth(){
       fill(100,255,0);
       ellipse(this.pos.x, this.pos.y, this.r * 2, this.r * 2);
     }
+
   }
-  
+
